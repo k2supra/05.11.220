4bb00d00fa4700e7c64751e93253ac77fcf792d7
andrii <andrijkorolevic@gmail.com>
2022-11-05 12:24:31 +0200

    Created new lab_ex_2

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("regrhthngerhrhgrthrhrtyjhyh")
+def ty(tt):
+    print(f"Somethings = {txt.count(sm)}")
+
+
+try:
+
+    print("Enter text")
+    txt = input("->")
+    print("Enter something")
+    sm = input("->")
+
+    def main():
+        ty(txt)
+
+    main()
+
+
+except Exception as ex:
+    print(f"Error, {ex}")
