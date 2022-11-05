65750d58a1bf420963dc5a06fba769d38cf156e9
andrii <andrijkorolevic@gmail.com>
2022-11-05 13:02:03 +0200

    Created new lab_ex_3

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("regrhthngerhrhgrthrhrtyjhyh")
+list = []
+while True:
+    try:
+        print("Enter number")
+        num = int(input("->"))
+
+        if num != 0:
+            list.append(num)
+            print("Sum = " + str(sum(list)))
+
+        else:
+
+            break
+
+
+
+
+    except Exception as ex:
+        print(f"Error: {ex}")
+print("Final sum = " + str(sum(list)))
